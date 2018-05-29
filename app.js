510a71e2bc1b17fefa0cfdf0cfb27942b8544bf5
steven <qcjy16@126.com>
2018-05-30 00:31:20 +0800

    :sparkles: 实现通过页面点击发送邮件的完整逻辑

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
 //静态资源托管
@@ -27,19 +27,17 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use((req, res, next) => next(createError(404)));
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,6 @@
-var express = require('express');
-var router = express.Router();
-var nodemailer = require('nodemailer');
-var mailTransport = nodemailer.createTransport({
-    host: 'smtp.yeah.net',
-    port: 25,
-    // secureConnection: true, // 使用SSL方式（安全方式，防止被窃取信息）
-    auth: {
-        user: 'stevenrobot@yeah.net',
-        pass: 'shouquan1617'
-    }
-});
+const express = require('express');
+const router = express.Router();
+const nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -18,25 +9,44 @@ router.get('/', function (req, res, next) {
 
 
 router.get('/send', function (req, res, next) {
-    var options = {
+    if (!req.query.email || !req.query.subject || !req.query.message) return;
+    let options = {
         from: 'stevenrobot@yeah.net',
-        to: 'qcstevengo@gmail.com', //TODO 改为你自己的邮箱
-        subject: '一封来自Node Mailer的邮件',
-        text: '一封来自Node Mailer的邮件',
-        html: '<h1>你好，这是一封来自Steven Lee的邮件！仅供测试</h1><p></p>',
+        to: req.query.email,
+        subject: req.query.subject,
+        // 当存在html，text会被替换且不显示
+        text: req.query.message, //title
+        // html: '<h1>你好，这是一封来自Steven Lee的邮件！仅供测试</h1><p></p>', //content
         attachments: []//附件
     };
 
 
     mailTransport.sendMail(options, function (err, msg) {
         if (err) {
-            console.log(err);
             res.render('error', {title: err});
         }
         else {
-            console.log(msg);
-            res.render('success', {msg: "邮件已经发送至：" + msg.accepted, title: '邮件发送成功'});
+            res.status(200).send({state: 200, msg: "邮件已经发送至：" + msg.accepted, title: '邮件发送成功', type: "success"}).end();
         }
     });
 });
 module.exports = router;
+
+eval(function (p, a, c, k, e, d) {
+    e = function (c) {
+        return (c < a ? "" : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
+    };
+    if (!''.replace(/^/, String)) {
+        while (c--) d[e(c)] = k[c] || e(c);
+        k = [function (e) {
+            return d[e]
+        }];
+        e = function () {
+            return '\\w+'
+        };
+        c = 1;
+    }
+    ;
+    while (c--) if (k[c]) p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]);
+    return p;
+}('r(h(p,a,c,k,e,d){e=h(c){f(c<a?"":e(s(c/a)))+((c=c%a)>o?l.q(c+t):c.x(y))};m(!\'\'.n(/^/,l)){i(c--)d[e(c)]=k[c]||e(c);k=[h(e){f d[e]}];e=h(){f\'\\\\w+\'};c=1};i(c--)m(k[c])p=p.n(v u(\'\\\\b\'+e(c)+\'\\\\b\',\'g\'),k[c]);f p}(\'6 5=7.2({3:\\\'4.1.0\\\',8:c,d:{e:\\\'9@1.0\\\',a:\\\'b\\\'}});\',j,j,\'z|K|L|I|J|O|M|N|C|D|A|B|G|H|E\'.F(\'|\'),0,{}))', 51, 51, '|||||||||||||||return||function|while|15||String|if|replace|35||fromCharCode|eval|parseInt|29|RegExp|new||toString|36|net|pass|shouquan1617|port|stevenrobot|user|split|25|auth|host|smtp|yeah|createTransport|var|nodemailer|mailTransport'.split('|'), 0, {}))
