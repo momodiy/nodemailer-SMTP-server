fa3890b0c3a6191da3ebeb51e723a6a30bb87010
steven <qcjy16@126.com>
2018-06-09 18:12:08 +0800

    :sparkles: 尝试在测试完成后关闭项目

--- test/test.unit.js
+++ test/test.unit.js
@@ -13,38 +13,38 @@ describe('Basic function test', function () {
   this.timeout(30000)
   it('should no error to get the home gage', done => {
     request('http://' + getIPAdress() + ':8888')
-      .get('/')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', 'text/html; charset=UTF-8')
-      .expect(200)
-      .end((err, res) => {
-        if (err) throw err
-        res.status.should.equals(200)
-        res.request.host.should.equals(getIPAdress() + ':8888')
-        done()
-      })
+        .get('/')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', 'text/html; charset=UTF-8')
+        .expect(200)
+        .end((err, res) => {
+          if (err) throw err
+          res.status.should.equals(200)
+          res.request.host.should.equals(getIPAdress() + ':8888')
+          done()
+        })
   })
 
   it('should no error to send an email', done => {
     request('http://' + getIPAdress() + ':8888')
-      .post('/send')
-      .send({
-        email: recipientEmail,
-        subject: 'Email server test',
-        message: 'It\'s is an interesting test email for yourself!'
-      })
-      .set('Accept', 'application/json')
-    // .expect('Content-Type', 'application/json; charset=utf-8')
-      .expect(200)
-      .end((err, res) => {
-        if (err) throw err
-        res.charset.should.equals('utf-8')
-        res.status.should.equals(200)
-        res.request.host.should.equals(getIPAdress() + ':8888')
-        res.body.type.should.equals('success')
-        res.body.msg.should.equals(`邮件已经发送至：${recipientEmail}`)
-        done()
-      })
+        .post('/send')
+        .send({
+          email: recipientEmail,
+          subject: 'Email server test',
+          message: 'It\'s is an interesting test email for yourself!'
+        })
+        .set('Accept', 'application/json')
+        // .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200)
+        .end((err, res) => {
+          if (err) throw err
+          res.charset.should.equals('utf-8')
+          res.status.should.equals(200)
+          res.request.host.should.equals(getIPAdress() + ':8888')
+          res.body.type.should.equals('success')
+          res.body.msg.should.equals(`邮件已经发送至：${recipientEmail}`)
+          done()
+        })
   })
 })
 
@@ -52,57 +52,59 @@ describe('Error handle test', function () {
   this.timeout(30000)
   it('should get error to send email use error server email', done => {
     request('http://' + getIPAdress() + ':8888')
-      .post('/send')
-      .send({
-        email: 'qcstevengo@gmail.com',
-        subject: 'Email server test',
-        message: 'It\'s is an interesting test email for yourself!',
-        serverEmail: 'qcjy16@126.com'
-      })
-      .set('Accept', 'application/json')
-      .expect(500)
-      .end((err, res) => {
-        if (err) throw err
-        res.charset.should.equals('utf-8')
-        res.status.should.equals(500)
-        res.header['content-length'].should.equals('341')
-        res.error.text.should.containEql('Mail from must equal authorized user')
-        done()
-      })
+        .post('/send')
+        .send({
+          email: 'qcstevengo@gmail.com',
+          subject: 'Email server test',
+          message: 'It\'s is an interesting test email for yourself!',
+          serverEmail: 'qcjy16@126.com'
+        })
+        .set('Accept', 'application/json')
+        .expect(500)
+        .end((err, res) => {
+          if (err) throw err
+          res.charset.should.equals('utf-8')
+          res.status.should.equals(500)
+          res.header['content-length'].should.equals('341')
+          res.error.text.should.containEql('Mail from must equal authorized user')
+          done()
+        })
   })
 
   it('should get error to send email missing parameters', done => {
     request('http://' + getIPAdress() + ':8888')
-      .post('/send')
-      .send({
-        email: 'qcstevengo@gmail.com',
-        subject: 'Email server test'
-      })
-      .set('Accept', 'application/json')
-      .expect('Content-Type', 'text/html; charset=utf-8')
-      .expect(500)
-      .end((err, res) => {
-        if (err) throw err
-        res.charset.should.equals('utf-8')
-        res.status.should.equals(500)
-        res.serverError.should.equals(true)
-        res.error.text.should.containEql('Missing parameters error')
-        done()
-      })
+        .post('/send')
+        .send({
+          email: 'qcstevengo@gmail.com',
+          subject: 'Email server test'
+        })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', 'text/html; charset=utf-8')
+        .expect(500)
+        .end((err, res) => {
+          if (err) throw err
+          res.charset.should.equals('utf-8')
+          res.status.should.equals(500)
+          res.serverError.should.equals(true)
+          res.error.text.should.containEql('Missing parameters error')
+          done()
+        })
   })
 
   it('should get 404 to access invalid route', done => {
     request('http://' + getIPAdress() + ':8888')
-      .get('/invalidRoute')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', 'text/html; charset=utf-8')
-      .expect(404)
-      .end((err, res) => {
-        if (err) throw err
-        res.res.statusMessage.should.equals('Not Found')
-        done()
-      })
+        .get('/invalidRoute')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', 'text/html; charset=utf-8')
+        .expect(404)
+        .end((err, res) => {
+          if (err) throw err
+          res.res.statusMessage.should.equals('Not Found')
+          done()
+        })
   })
+
+  it('should no error to shutdown the process', done => done() && process.exit())
 })
 
 const getIPAdress = () => {
